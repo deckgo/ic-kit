758b55daf47d2a1c46c68e2b06b88fd918244734
peterpeterparker <david.dalbusco@outlook.com>
2021-11-26 08:09:29 +0100

    feat: split js and css in separate files

--- build.js
+++ build.js
@@ -3,17 +3,16 @@ const {readFile, writeFile, mkdir, rm} = require('fs').promises;
 const minify = require('html-minifier-terser').minify;
 const crypto = require('crypto');
 
-(async () => {
-  await rm('./dist', {recursive: true, force: true});
-  await mkdir('./dist');
-
-  const {outputFiles: js} = esbuild.buildSync({
+const buildScript = () => {
+  const {metafile} = esbuild.buildSync({
     entryPoints: ['src/deck/index.js'],
+    entryNames: '[dir]/[name]-[hash]',
     bundle: true,
     minify: true,
     format: 'esm',
     target: ['esnext'],
-    write: false,
+    outdir: 'dist/build/deck',
+    metafile: true,
     define: {
       'process.env.SIGNALING_SERVER': JSON.stringify('https://api.deckdeckgo.com'),
       'process.env.NO_REMOTE': 'false',
@@ -21,19 +20,41 @@ const crypto = require('crypto');
     }
   });
 
-  const script = js[0].text;
+  const {outputs} = metafile;
+  return Object.keys(outputs)[0];
+};
+
+const scriptSha256 = async (scriptPath) => {
+  const script = await readFile(scriptPath, 'utf8');
 
   // prettier-ignore
-  const sha256 = `'sha256-${crypto.createHash("sha256").update(script).digest("base64")}'`;
+  return `'sha256-${crypto.createHash("sha256").update(script).digest("base64")}'`;
+};
 
-  const {outputFiles: css} = esbuild.buildSync({
-    entryPoints: ['src/deck/index.css'],
+const buildCSS = () => {
+  const {metafile} = esbuild.buildSync({
+    entryPoints: ['src/index.css', 'src/deck/index.css'],
     bundle: true,
     minify: true,
     format: 'esm',
     target: ['esnext'],
-    write: false
+    outdir: 'dist/build',
+    metafile: true
   });
+
+  const {outputs} = metafile;
+  return Object.keys(outputs);
+};
+
+(async () => {
+  await rm('./dist', {recursive: true, force: true});
+  await mkdir('./dist/p', {recursive: true});
+
+  const scriptPath = buildScript();
+
+  const sha256 = await scriptSha256(scriptPath);
+
+  const cssPaths = buildCSS();
 
   const src = await readFile('src/deck/index.html', 'utf8');
 
@@ -51,8 +72,8 @@ const crypto = require('crypto');
 
   const html = (await minify(src, minifyOptions))
     .replace('{{DECKDECKGO_EXTRA_SHAS}}', sha256)
-    .replace('<!-- DECKDECKGO_HEAD_SCRIPT -->', `<script>${script}</script>`)
-    .replace('<!-- DECKDECKGO_HEAD_CSS -->', `<style>${css[0].text}</style>`);
+    .replace('<!-- DECKDECKGO_HEAD_SCRIPT -->', `<script type="module" src="${scriptPath.replace('dist', '')}"></script>`)
+    .replace('<!-- DECKDECKGO_HEAD_CSS -->', cssPaths.map(cssPath => `<link href="${cssPath.replace('dist', '')}" rel="stylesheet">`).join(''));
 
-  await writeFile('dist/deck.html', html);
+  await writeFile('dist/p/deck.html', html);
 })();
